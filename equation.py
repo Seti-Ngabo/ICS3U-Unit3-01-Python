408fa44c6c095da21a0ddcced139ae3285d6d338
Ubuntu <ubuntu@ip-172-31-51-232.ec2.internal>
2021-09-16 16:41:58 +0000

    added file

---
+++ equation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created by: Seti Ngabo
+# Created on: Sept 2021
+# This program adds the first number and the second number
+#   with user input
+
+
+def main():
+    # input
+    first_number = int(input("Enter first number (integer): "))
+    second_number = int(input("Enter second number (integer): "))
+
+    # process
+    answer = first_number + second_number 
+
+    # output
+    print("{0} + {1} = {2} ".format(first_number, second_number, answer))
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
